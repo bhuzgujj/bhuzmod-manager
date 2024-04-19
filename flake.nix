cfb081d91265828f670087f06fdd617530f00ec5
Émile Boucher <emile.boucher@hotmail.fr>
2024-04-18 20:10:12 -0400

    workin on nixos

--- flake.nix
+++ flake.nix
@@ -1,33 +1,49 @@
-let
-  pkgs = import <nixpkgs> { };
+{
+  description = "Tauri builder";
 
-  libraries = with pkgs;[
-    webkitgtk
-    gtk3
-    cairo
-    gdk-pixbuf
-    glib
-    dbus
-    openssl_3
-  ];
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
 
-  packages = with pkgs; [
-    pkg-config
-    dbus
-    openssl_3
-    glib
-    gtk3
-    libsoup
-    webkitgtk
-    appimagekit
-  ];
-in
-pkgs.mkShell {
-  buildInputs = packages;
+  outputs = { self, nixpkgs, flake-utils }:
+      flake-utils.lib.eachDefaultSystem (system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
 
-  shellHook =
-    ''
-      export LD_LIBRARY_PATH=${pkgs.lib.makeLibraryPath libraries}:$LD_LIBRARY_PATH
-      export XDG_DATA_DIRS=${pkgs.gsettings-desktop-schemas}/share/gsettings-schemas/${pkgs.gsettings-desktop-schemas.name}:${pkgs.gtk3}/share/gsettings-schemas/${pkgs.gtk3.name}:$XDG_DATA_DIRS
-    '';
+          libraries = with pkgs;[
+            webkitgtk
+            gtk3
+            cairo
+            gdk-pixbuf
+            glib
+            dbus
+            openssl_3
+            librsvg
+          ];
+
+          packages = with pkgs; [
+            curl
+            wget
+            pkg-config
+            dbus
+            openssl_3
+            glib
+            gtk3
+            libsoup
+            webkitgtk
+            librsvg
+          ];
+        in
+        {
+          devShell = pkgs.mkShell {
+            buildInputs = packages;
+
+            shellHook =
+              ''
+                export LD_LIBRARY_PATH=${pkgs.lib.makeLibraryPath libraries}:$LD_LIBRARY_PATH
+                export XDG_DATA_DIRS=${pkgs.gsettings-desktop-schemas}/share/gsettings-schemas/${pkgs.gsettings-desktop-schemas.name}:${pkgs.gtk3}/share/gsettings-schemas/${pkgs.gtk3.name}:$XDG_DATA_DIRS
+              '';
+          };
+        });
 }
